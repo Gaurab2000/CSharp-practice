846389b2b47efa359fb9f8ec019348941be7efb3
Gaurab2000 <106250922+Gaurab2000@users.noreply.github.com>
2022-05-25 19:15:15 +0545

    c#

---
+++ DATAencapsulation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class DemoEncap
+{
+    private String studentName;
+    private int studentAge;
+
+    public String Name
+    {
+        get { return studentName; }
+        set { studentName = value; }
+    }
+    public int Age
+    {
+        get { return studentAge; }
+        set { studentAge = value; }
+    }
+}
+
+class Gaurab
+{
+    static public void Main()
+    {
+        DemoEncap obj = new DemoEncap();
+        obj.Name = "GAURAB";
+        obj.Age = 21;
+        Console.WriteLine("Name:" + obj.Name);
+        Console.WriteLine("Age:" + obj.Age);
+        Console.ReadLine();
+    }
+}
