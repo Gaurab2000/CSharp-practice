ce7b8ac9222c21bc39b54ee10640a09af9634e24
Gaurab2000 <106250922+Gaurab2000@users.noreply.github.com>
2022-05-25 19:24:28 +0545

    c#

---
+++ inheritance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Gau
+    {
+        public string name;
+        public string subject;
+
+        public void readers(string name, string subject)
+        {
+            this.name = name;
+            this.subject = subject;
+            Console.WriteLine("MYSELF:" + name);
+            Console.WriteLine("my favoutrite subject is :" + subject);
+
+        }
+
+    }
+    class Rab : Gau
+    {
+        public void outputtext()
+        {
+            Console.WriteLine("C sharp");
+
+        }
+    }
+
+    class Final
+    {
+        
+        static void Main(string[] args
+            )
+        {
+            Rab r = new Rab();
+            r.readers("Gaurab", "c sharp");
+            Console.ReadLine();
+        }
+    }
+
+}
