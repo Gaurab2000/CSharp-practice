a0657f5df9342a91fbc315d98f38ed00452501d6
Gaurab Rayamajhi <106250922+Gaurab2000@users.noreply.github.com>
2022-05-30 15:01:22 +0545

    Add files via upload

---
+++ indexoutofrangeexception.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace labreport { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            try
+            {
+                arr[0] = 11;
+                arr[1] = 12;
+                arr[2] = 13;
+                arr[3] = 14;
+                foreach (int i in arr)
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+            catch(IndexOutOfRangeException ex)
+
+            {
+                Console.WriteLine("Array's range is exceede...");
+                Console.WriteLine(ex.Message);
+            }
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+                
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
